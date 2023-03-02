c51636a4f50872a926bea19eba9ce63ccf3eeae8
Joshua Keith <joshualeekeith@outlook.com>
2023-03-01 21:10:04 -0600

    Whack a mole game

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 let score = 0;
+
+let scoreDisplay = document.getElementById("score");
+
+let holes = document.getElementsByClassName("hole");
+//console.log(document.getElementsByClassName("hole"));
+setInterval(function () {
+  let randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle("mole");
+}, 300);
+
+let gameArea = document.getElementById("whack-a-mole");
+gameArea.addEventListener("click", function (clickEvent) {
+  if (clickEvent.target.matches(".mole")) {
+    clickEvent.target.classList.remove("mole");
+    score++;
+    scoreDisplay.innerText = score;
+    // we hit a mole
+  }
+});
